f3f2253f080833eac40ad9e576d2bd6ea0dfcdec
Mawuli Azameti <60759789+ikmazameti@users.noreply.github.com>
2024-08-27 22:07:55 +0000

    first commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,85 +1,48 @@
 /**
- *  Control Flow using Conditionals (if-else and when)
+ * For Loops
  */
+
 fun main() {
 
+println(1)
+println(2)
+println(3)
+println(4)
+println(5)
 
+
+
+
+
+
+    //Syntax
     /**
-     *1. Syntax of if Statement
-     * ***********************
-     *
-     *   if (condition){
-     *      body
-     *   }
-     */
+     * for (item in collection){
+     *   //code
+     * }
+    * */
 
-    val age = 30
-    if (age >= 18) println("You are eligible to vote!") else println("You are not eligible to vote!")
+//    Examples 1:
 
 
-    val voterAge = 70
-    if (voterAge >= 65) {
-        println("You can vote and you are eligible for senior citizen discount!")
-    } else if (voterAge >= 18) {
-        println("You can vote but  you are not eligible for senior citizen discount!")
-    } else {
-        println("You can not vote and you are not eligible for senior citizen discount!")
+//    for (item in collection ){
+//
+//    }
+
+    println("-------------------")
+    for (i in 1..5 ){
+        println(i)
     }
 
 
-    /**
-     * Syntax of if-else Statement
-     * ***********************
-     *
-     *   if (condition){
-     *      body1
-     *   } else {
-     *      body2
-     *   }
-     */
+
+    //range
+    //start value .. end value
 
 
-    val firstNum = 20
-    val secondNum = 30
-    val max = if (firstNum > secondNum) firstNum else secondNum
+    //collection
+    val fruits = arrayOf("orange","banana", "guava")
 
-
-    println("The maximum is : $max")
-
-
-    /**
-     *2. Syntax of when Statement
-     * ***********************
-     *
-     *   when (parameter){
-     *     condition1 -> body1
-     *     condition2 -> body2
-     *     condition3 -> body3
-     *   }
-     */
-
-    val trafficLightColor = "Purple"
-    when (trafficLightColor) {
-        "Red" -> {
-            println("Stop")
-            println("Blinking")
-        }
-        "Yellow" -> println("Go slow")
-        "Green" -> println("Go")
-
-        else -> println("Invalid color")
-    }
-
-
-    //when expression
-    val action = when(trafficLightColor){
-        "Red" -> "Stop"
-        "Yellow" -> "Get ready"
-        "Green" -> "Go"
-        else -> "Invalid color"
-    }
-
-    println(action  )
-
-
+    for (fruit in fruits)
+        println(fruit)
 }
